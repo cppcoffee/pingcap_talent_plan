bfbe5466ede721ea1de70d4db5778bf0d78d2499
sharp <cppcoffee@gmail.com>
2019-08-15 22:42:45 +0800

    Project-1: use structopt crate.

--- week-1/project-1/src/bin/kvs.rs
+++ week-1/project-1/src/bin/kvs.rs
@@ -1,51 +1,31 @@
-use clap::{App, AppSettings, Arg, SubCommand};
 use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "kvs", about = "kvs project")]
+enum Opt {
+    #[structopt(name = "set", about = "Set the value of a string key to a string")]
+    Set { key: String, value: String },
+    #[structopt(name = "get", about = "Get the string value of a given string key")]
+    Get { key: String },
+    #[structopt(name = "rm", about = "Remove a given key")]
+    Rm { key: String },
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove a given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .get_matches();
+    let opt = Opt::from_args();
 
-    match matches.subcommand() {
-        ("set", Some(_matches)) => {
-            eprintln!("unimplemented!");
+    match opt {
+        Opt::Set { key: _k, value: _v } => {
+            eprintln!("unimplemented");
             exit(1);
         }
-        ("get", Some(_matches)) => {
-            eprintln!("unimplemented!");
+        Opt::Get { key: _k } => {
+            eprintln!("unimplemented");
             exit(1);
         }
-        ("rm", Some(_matches)) => {
-            eprintln!("unimplemented!");
-            exit(1);
-        }
-        _ => {
-            eprintln!("unimplemented!");
+        Opt::Rm { key: _k } => {
+            eprintln!("unimplemented");
             exit(1);
         }
     }
